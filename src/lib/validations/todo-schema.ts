27e9d267b846502aa3811a1970d3b4f768bef8be
lalatina11 <rahmadancandra@gmail.com>
2026-03-06 09:04:29 +0700

    refactor: using old-school way for zod validation

--- src/lib/validations/todo-schema.ts
+++ src/lib/validations/todo-schema.ts
@@ -1,8 +1,9 @@
-import { createInsertSchema } from "drizzle-zod";
 import z from "zod";
-import { todo } from "@/db/schema";
 
-export const todoSchema = createInsertSchema(todo);
+export const todoSchema = z.object({
+	title: z.string().min(3, { message: "Title must be at least 3 characters" }),
+	description: z.string().optional(),
+});
 
 export type TodoSchemaType = z.infer<typeof todoSchema>;
 
